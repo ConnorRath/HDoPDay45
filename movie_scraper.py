fc16cc963e7e822e72a8727980d2643636cf80cc
ConnorRath <ConnorRath@users.noreply.github.com>
2023-05-07 14:39:38 -0500

    Update movie_scraper.py

--- movie_scraper.py
+++ movie_scraper.py
@@ -1,6 +1,4 @@
 from bs4 import BeautifulSoup
-import numpy
-import pprint
 import requests
 import json
 
@@ -9,12 +7,6 @@ req = requests.get(url)
 soup = BeautifulSoup(req.content, "html.parser")
 script = soup.find("script", type='application/json')
 data = json.loads(script.string)
-#print(soup.prettify())
-movie_title = soup.select_one(selector="body div main article.jsx-1456218620 .article-content.jsx-1456218620 "
-                                       ".listicle-container.jsx-3523802742 .listicle-item.jsx-3523802742")
-titles = soup("body div main article.jsx-1456218620 .article-content.jsx-1456218620 "
-                                       ".listicle-container.jsx-3523802742 .listicle-item.jsx-3523802742")
-# print(movie_title)
 movie_list = []
 
 for film in data["props"]["pageProps"]["data"]["getArticleByFurl"]["_layout"][3]['content']['images']:
