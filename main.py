5e80f2b5f87b83b43a762f986ec3eafc7a893e56
ConnorRath <ConnorRath@users.noreply.github.com>
2023-05-06 21:09:54 -0500

    Moving through day 45 HDoP. Still a lot to go

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import lxml
+
+with open("website.html", encoding="utf-8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+
+all_anchor_tags = soup.find_all(name="a")
+print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    print(tag.getText())
